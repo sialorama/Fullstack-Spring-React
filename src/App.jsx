6ffcd4952c0e016e03b4816b0e45965ded38ce76
sialorama <sialorama@gmail.com>
2024-09-05 17:01:05 +0200

    update adduser.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,25 @@ import './App.css'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
 import Navbar from './layout/Navbar';
+import { Router, Routes, Route } from 'react-router-dom';
+import { AddUser } from './users/AddUser';
+
+
 
 function App() {
 
   return (
 
     <div className='App'>
-    <Navbar />
-      <Home />
-
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element ={<Home/>} />
+          <Route exact path="/adduser" element ={<AddUser/>} />
+          <Route exact path="/edituser" element ={<EditUser/>} />
+          <Route exact path="/viewuser" element ={<ViewUser/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/users/ViewUser.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ViewUser = () => {
+  return (
+    <div>ViewUser</div>
+  )
+}
